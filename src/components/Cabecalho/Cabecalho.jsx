044628c1c204fa8c58a390a380e406dace2f9fe2
vcamargo04 <viniciusac2005@Hotmail.com>
2023-11-23 00:33:47 -0300

    Alterações no cabecalho.

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,19 +1,27 @@
-
-
+// components/Header.jsx
 import Link from 'next/link';
 import styles from '../../styles/cabecalho.module.css';
 
 const Header = ({ currentPath }) => {
-  const getOppositePath = (path) => {
-    switch (path) {
-      case '/login':
-        return '/cadastro';
-      default:
-        return '/login';
-    }
-  };
+  const isHome = currentPath === '/';
+  let oppositePath1, oppositePath2, link1Text, link2Text;
 
-  const oppositePath = getOppositePath(currentPath);
+  if (currentPath === '/login') {
+    oppositePath1 = '/cadastro';
+    oppositePath2 = '/';
+    link1Text = isHome ? 'Cadastro' : 'Cadastro';
+    link2Text = isHome ? 'Home' : 'Home';
+  } else if (currentPath === '/cadastro') {
+    oppositePath1 = '/login';
+    oppositePath2 = '/';
+    link1Text = isHome ? 'Login' : 'Login';
+    link2Text = isHome ? 'Home' : 'Home';
+  } else {
+    oppositePath1 = '/login';
+    oppositePath2 = '/cadastro';
+    link1Text = 'Login';
+    link2Text = 'Cadastro';
+  }
 
   return (
     <header className={styles.header}>
@@ -21,12 +29,22 @@ const Header = ({ currentPath }) => {
         <img src="/logoNotreDame.jpeg" alt="Logo" />
       </div>
       <nav className={styles.nav}>
-        <Link href={oppositePath}>
-          <div>{oppositePath === '/cadastro' ? 'Cadastro' : 'Login'}</div>
+        <Link href={oppositePath1}>
+          <div>{link1Text}</div>
         </Link>
-        <Link href="/">
-          <div>Home</div>
+        <Link href={oppositePath2}>
+          <div>{link2Text}</div>
         </Link>
+        {isHome && (
+          <>
+            <Link href="/cadastro">
+              <div>Cadastro</div>
+            </Link>
+            <Link href="/login">
+              <div>Login</div>
+            </Link>
+          </>
+        )}
       </nav>
     </header>
   );
